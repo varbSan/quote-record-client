893665494dbe948d06412a0036c47e64d4cffd29
Bakary <var.bsan@gmail.com>
2025-03-25 13:28:48 +0100

    chore

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv {
-  readonly VITE_API_URL: string
-  readonly VITE_NODE_ENV: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
