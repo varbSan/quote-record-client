3cd6a0c82f8d092c44bd082d87094acd11c68a23
Bakary <var.bsan@gmail.com>
2025-03-25 16:35:14 +0100

    fix codegen watcher

--- codegen.ts
+++ codegen.ts
@@ -6,13 +6,16 @@ const isDevelopment = process.env.VITE_NODE_ENV === 'development'
 
 const config: CodegenConfig = {
   overwrite: true,
-  watch: isDevelopment,
   schema: isDevelopment ? process.env.VITE_API_URL : './graphql.schema.json',
-  documents: ['src/**/*.query.ts', 'src/**/*.mutation.ts', 'src/**/*.subscription.ts'],
+  documents: ['src/**/*.{query,mutation,subscription}.ts'],
+  ignoreNoDocuments: true,
   generates: {
     'src/gql/': {
       preset: 'client',
       plugins: [],
+      config: {
+        useTypeImports: true,
+      },
     },
     ...(isDevelopment && {
       './graphql.schema.json': {
