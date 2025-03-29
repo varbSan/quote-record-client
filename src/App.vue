35b94dde9d2d7208ee4e7e74071b15e0d2069f32
Bakary <var.bsan@gmail.com>
2025-03-29 19:14:00 +0100

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,66 @@
 <script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui'
+import { useToast } from '@nuxt/ui/runtime/composables/useToast.js'
 import { useMutation, useQuery, useSubscription } from '@vue/apollo-composable'
-import { ref } from 'vue'
+import * as v from 'valibot'
+import { computed, reactive, ref, watch } from 'vue'
+
 import { RouterView } from 'vue-router'
 import { CREATE_QUOTE_RECORD_MUTATION } from './api/apollo/mutations/createQuoteRecord.mutation'
 import { GET_LAST_QUOTE_RECORD_QUERY } from './api/apollo/queries/getLastQuoteRecord.query'
 import { QUOTE_RECORD_CREATED_SUBSCRIPTION } from './api/apollo/subscriptions/quoteRecordCreated.subscription'
 
-const { result, loading: queryLoading, refetch, error: resultError } = useQuery(GET_LAST_QUOTE_RECORD_QUERY)
-const { mutate, loading: mutationLoading, error: mutationError } = useMutation(CREATE_QUOTE_RECORD_MUTATION)
+const { result: resultQuery, loading: queryLoading, refetch } = useQuery(GET_LAST_QUOTE_RECORD_QUERY)
 const { result: resultSubscription } = useSubscription(QUOTE_RECORD_CREATED_SUBSCRIPTION)
+const { mutate, loading: mutationLoading, error: mutationError } = useMutation(CREATE_QUOTE_RECORD_MUTATION)
 
-const fooError = ref()
+const toast = useToast()
 
-async function handleCreate() {
+const lastTextQuote = computed(() => resultSubscription.value?.quoteRecordCreated.text ?? resultQuery.value?.getLastQuoteRecord.text ?? '')
+const schema = v.object({
+  quoteRecordText: v.pipe(v.string(), v.minLength(8, 'Must be at least 8 characters')),
+})
+
+type Schema = v.InferOutput<typeof schema>
+
+const state = reactive({
+  quoteRecordText: '',
+})
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
-    await mutate({ text: 'Hello World' })
+    await mutate({ text: event.data.quoteRecordText })
+    toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
   }
   catch (err) {
-    fooError.value = resultError.value ?? mutationError.value ?? err
+    toast.add({ title: 'Error', description: mutationError.value?.message ?? err?.toString() ?? '', color: 'error' })
   }
 }
+const subscriptionText = ref()
+watch(resultSubscription, () => {
+  subscriptionText.value = resultSubscription.value?.quoteRecordCreated.text
+})
 </script>
 
 <template>
   <UApp>
     <RouterView />
     <br>
-    {{ resultSubscription?.quoteRecordCreated.text }}
+    {{ lastTextQuote }}
     <br>
 
-    <UButton :loading="mutationLoading" @click="handleCreate">
-      create
-    </UButton>
+    <UForm :schema="schema" :state="state" class="space-y-4 border-2 p-4 m-4" @submit="onSubmit">
+      <UFormField label="quote" name="quoteRecordText">
+        <UInput v-model="state.quoteRecordText " />
+      </UFormField>
+
+      <UButton :loading="mutationLoading" type="submit">
+        create
+      </UButton>
+    </UForm>
     <br>
     <UButton :loading="queryLoading" @click="refetch">
       refetch
     </UButton>
-    <br>
-    {{ result?.getLastQuoteRecord }}
-    <br>
-    <br>
-    {{ fooError?.name ?? '' }}
-    {{ fooError?.message ?? '' }}
-    {{ fooError?.stack ?? '' }}
   </UApp>
 </template>
