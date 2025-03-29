bdf0cd59bcb1447ed025c3f5c2407f5f21938cd0
Bakary <var.bsan@gmail.com>
2025-03-29 12:10:24 +0100

    chore

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import { RouterView } from 'vue-router'
 import { CREATE_QUEUE_POINT_MUTATION } from './api/apollo/mutations/createQueuePoint.mutation'
 import { GET_LAST_QUEUE_POINT_QUERY } from './api/apollo/queries/getLastQueuePoint.query'
 
-const { result, loading: queryLoading, refetch } = useQuery(GET_LAST_QUEUE_POINT_QUERY)
-const { mutate, loading: mutationLoading } = useMutation(CREATE_QUEUE_POINT_MUTATION)
+const { result, loading: queryLoading, refetch, error: resultError } = useQuery(GET_LAST_QUEUE_POINT_QUERY)
+const { mutate, loading: mutationLoading, error: mutationError } = useMutation(CREATE_QUEUE_POINT_MUTATION)
 
 const fooError = ref()
 
@@ -15,7 +15,7 @@ async function handleCreate() {
     await mutate({ row: 4 }, { update: cache => cache.evict({ fieldName: 'getLastQueuePoint' }) })
   }
   catch (err) {
-    fooError.value = err
+    fooError.value = resultError.value ?? mutationError.value ?? err
   }
 }
 </script>
@@ -31,6 +31,8 @@ async function handleCreate() {
     <UButton :loading="queryLoading" @click="refetch">
       refetch
     </UButton>
-    {{ fooError }}
+    {{ fooError.name }}
+    {{ fooError.message }}
+    {{ fooError.stack }}
   </UApp>
 </template>
