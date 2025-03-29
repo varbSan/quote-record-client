d57850598a52e66abd540c3c33185e867f5cd9dc
Bakary <var.bsan@gmail.com>
2025-03-29 13:35:53 +0100

    implement subscription for testing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import { useMutation, useQuery } from '@vue/apollo-composable'
+import { useMutation, useQuery, useSubscription } from '@vue/apollo-composable'
 import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import { CREATE_QUEUE_POINT_MUTATION } from './api/apollo/mutations/createQueuePoint.mutation'
 import { GET_LAST_QUEUE_POINT_QUERY } from './api/apollo/queries/getLastQueuePoint.query'
+import { QUEUE_POINT_CREATED_SUBSCRIPTION } from './api/apollo/subscriptions/queuePointCreated.subscription'
 
 const { result, loading: queryLoading, refetch, error: resultError } = useQuery(GET_LAST_QUEUE_POINT_QUERY)
 const { mutate, loading: mutationLoading, error: mutationError } = useMutation(CREATE_QUEUE_POINT_MUTATION)
+const { result: resultSubscription } = useSubscription(QUEUE_POINT_CREATED_SUBSCRIPTION)
 
 const fooError = ref()
 
 async function handleCreate() {
   try {
-    await mutate({ row: 4 }, { update: cache => cache.evict({ fieldName: 'getLastQueuePoint' }) })
+    await mutate({ row: 4 })
   }
   catch (err) {
     fooError.value = resultError.value ?? mutationError.value ?? err
@@ -24,13 +26,20 @@ async function handleCreate() {
   <UApp>
     <RouterView />
     <br>
-    {{ result?.getLastQueuePoint }}
+    {{ resultSubscription?.queuePointCreated.id }}
+    <br>
+
     <UButton :loading="mutationLoading" @click="handleCreate">
       create
     </UButton>
+    <br>
     <UButton :loading="queryLoading" @click="refetch">
       refetch
     </UButton>
+    <br>
+    {{ result?.getLastQueuePoint }}
+    <br>
+    <br>
     {{ fooError?.name ?? '' }}
     {{ fooError?.message ?? '' }}
     {{ fooError?.stack ?? '' }}
