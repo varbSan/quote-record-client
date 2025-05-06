5d3406c34978d145a40a34fa36dd26fe51cc9480
Bakary <var.bsan@gmail.com>
2025-05-06 20:14:51 +0200

    fix section outline

--- src/pages/PgAbout/views/VwPrivacyPolicy.vue
+++ src/pages/PgAbout/views/VwPrivacyPolicy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="space-y-4 outline-1 md:outline-[0.5px] outline-white/40 rounded-lg p-4">
+  <div class="space-y-4 outline-1 outline-black/15 dark:outline-white/40 rounded-lg p-4">
     Privacy Policy
   </div>
 </template>
