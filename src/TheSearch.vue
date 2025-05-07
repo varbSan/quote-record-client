62328047b3e2275fff1c6f338249b804ccaea77a
Bakary <var.bsan@gmail.com>
2025-05-07 11:04:10 +0200

    remove routes from search

--- src/TheSearch.vue
+++ src/TheSearch.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { CommandPaletteItem } from '@nuxt/ui'
-import type { RouteLocationMatched, RouteLocationNormalizedLoadedGeneric, RouteRecordRaw } from 'vue-router'
 import type { GetQuotesQuery } from './gql/graphql'
 import { useSession } from '@clerk/vue'
 import { defineShortcuts } from '@nuxt/ui/runtime/composables/defineShortcuts.js'
@@ -8,7 +7,6 @@ import { useLazyQuery } from '@vue/apollo-composable'
 import { computed, ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { GET_QUOTES_QUERY } from './api/apollo/queries/getQuotes.query'
-import { routes } from './router/routes'
 
 const props = withDefaults(defineProps<{
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
@@ -35,12 +33,6 @@ const searchItems = computed(() => [{
   id: 'quotes',
   label: searchTerm.value ? `Quotes matching “${searchTerm.value}”...` : 'Quotes',
   items: result.value?.getQuotes.map(quote => formatQuoteItem(quote)) ?? [],
-}, {
-  id: 'routes',
-  label: searchTerm.value ? `Routes matching “${searchTerm.value}”...` : 'Routes',
-  items: routes
-    .filter(route => !!route.name && route.name !== 'signin' && isSignedIn.value)
-    .map(route => formatRouteItem(route, route.children)),
 }])
 
 defineShortcuts({
@@ -48,21 +40,6 @@ defineShortcuts({
     isSearchModalOpen.value = !isSearchModalOpen.value
   },
 })
-
-function formatRouteItem(
-  route: RouteRecordRaw | RouteLocationNormalizedLoadedGeneric | RouteLocationMatched,
-  siblings: RouteRecordRaw[] = [],
-): CommandPaletteItem {
-  const children = siblings.map(sibling => formatRouteItem(sibling))
-  return {
-    slot: 'dropdown' as const,
-    label: route.meta?.label as string,
-    value: route.name as string,
-    icon: route.meta?.icon as string,
-    to: { name: route.name },
-    children,
-  }
-}
 
 function formatQuoteItem(quote: GetQuotesQuery['getQuotes'][number]): CommandPaletteItem {
   return {
